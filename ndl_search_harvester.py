f3d49ea57b57ac3af6a6d5954118bfb53f482cac
nakamura-akifumi <werdna4939>
2017-03-18 22:50:13 +0900

    日付の指定に対応した。

--- ndl_search_harvester.py
+++ ndl_search_harvester.py
@@ -2,6 +2,7 @@ import urllib.request
 import requests
 import os.path
 from lxml import etree
+from datetime import *
 
 # lxml package for windows (http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml)
 # get lxml‑3.7.3‑cp36‑cp36m‑win_amd64.whl
@@ -9,18 +10,22 @@ from lxml import etree
 
 # references
 # http://ailaby.com/ndl_search/
-# http://ymotongpoo.hatenablog.com/entry/20110729/1311900747
 
-
-def fetch_from_ndl():
+def fetch_from_ndl(date_from, date_until):
     base_url = "http://iss.ndl.go.jp/api/oaipmh"
 
     params = {}
     params['verb'] = "ListRecords"
     params['metadataPrefix'] = "dcndl"
     params['set'] = "iss-ndl-opac-national"
-    params['from'] = "2013-07-15"
-    params['until'] = "2013-07-16"
+
+    if date_from is None:
+        date_from = datetime.today()
+    if date_until is None:
+        date_until = datetime.today() + 1
+
+    params['from'] = date_from.strftime('%Y-%m-%d')
+    params['until'] = date_until.strftime('%Y-%m-%d')
 
     print(f"start request {base_url}")
 
@@ -42,7 +47,7 @@ def fetch_from_ndl():
 
         # store
         folder = "c:\\tmp"
-        filename = f"ndl_oaipmh_{index}.xml"
+        filename = f"ndl_oaipmh_{params['from']}-{index}.xml"
         writepath = os.path.join(folder, filename)
 
         with open(writepath, 'wb') as f:
@@ -58,7 +63,7 @@ def fetch_from_ndl():
             x = resumption_tokens[0]
             params['resumptionToken'] = x.text
 
-        if resumption_tokens == None or len(resumption_tokens) == 0 or params['resumptionToken'] == None:
+        if resumption_tokens is None or len(resumption_tokens) == 0 or params['resumptionToken'] is None:
             break
 
         index = index + 1
@@ -70,4 +75,7 @@ def fetch_from_ndl():
 
 
 if __name__ == '__main__':
-    fetch_from_ndl()
+    # tdatetime_from = datetime.now()
+    tdatetime_from = datetime(2013, 7, 14)
+    tdatetime_until = tdatetime_from + timedelta(days=1)
+    fetch_from_ndl(tdatetime_from, tdatetime_until)
